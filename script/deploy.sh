cb62b211b31c3722fc177d903d888157f4ad7d1c
pramodpra <pramod.luffy@gmail.com>
2021-02-03 19:43:46 +0530

    nvm added

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,6 +7,7 @@ ssh ubuntu@52.207.122.203 <<EOF
     git pull origin main
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
+    source ~/.nvm/nvm.sh
     nvm install v12.18.3
     npm install
     npm install -g nodemon pm2
