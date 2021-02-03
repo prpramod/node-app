4f8b13bf847d8d5bbdc65313ff5bd4480939f891
pramodpra <pramod.luffy@gmail.com>
2021-02-03 18:51:19 +0530

    pwd added

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,7 @@
  
 ssh ubuntu@52.207.122.203 <<EOF
     cd ~/node-app
+    pwd
     git pull origin main
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
