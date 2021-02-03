cbef79f6ba8d7e5dfdc4bccb8e974880f7d205c5
pramodpra <pramod.luffy@gmail.com>
2021-02-03 15:58:09 +0530

    add deployment script

---
+++ script/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ssh ubuntu@52.207.122.203 <<EOF
+    cd ~/node-app
+    git pull origin main
+    curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
+    . ~/.nvm/nvm.sh
+    nvm install v12.18.3
+    npm install
+    npm install -g nodemon pm2
+    pm2 restart ecosystem.config.js
+    exit
+EOF
